9eb4e390e972b24640432e47fa0302f82ae94b64
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-09 21:29:58 +0530

    Initial commit

---
+++ src/main/java/com/attraya/SpringbootTransactionDemoV2Application.java
@@ -0,0 +1,13 @@
+package com.attraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemoV2Application.class, args);
+	}
+
+}
