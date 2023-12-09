20a74ffcc0d2b35ab013e987a86e4082407e8e60
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-09 22:03:54 +0530

    Add DTOs

---
+++ src/main/java/com/attraya/dto/FlightBookingAcknowledgement.java
@@ -0,0 +1,17 @@
+package com.attraya.dto;
+
+import com.attraya.entity.PassengerInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightBookingAcknowledgement {
+
+    private String status;
+    private double totalFare;
+    private String pnrNo;
+    private PassengerInfo passengerInfo;
+}
---
+++ src/main/java/com/attraya/dto/FlightBookingRequest.java
@@ -0,0 +1,15 @@
+package com.attraya.dto;
+
+import com.attraya.entity.PassengerInfo;
+import com.attraya.entity.PaymentInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightBookingRequest {
+    private PassengerInfo passengerInfo;
+    private PaymentInfo paymentInfo;
+}
