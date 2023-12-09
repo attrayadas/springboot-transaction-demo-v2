265097bd23e56e78dd432290e6687bff60ab59d9
Attraya Das <attrayaghoshdas@gmail.com>
2023-12-09 21:48:22 +0530

    Add Entity classes

---
+++ src/main/java/com/attraya/entity/PaymentInfo.java
@@ -0,0 +1,27 @@
+package com.attraya.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PAYMENT_INFO")
+public class PaymentInfo {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerated")
+    private String paymentId;
+    private String accountNo;
+    private Double amount;
+    private String cardType;
+    private Long passengerId;
+}
